c70a80ecfe1200fc650e6c072be54b3681070172
TerryLee7788 <k9114658@gmail.com>
2015-07-31 19:11:39 +0800

    update

--- test2.js
+++ test2.js
@@ -30,13 +30,13 @@ var lib_list = require('./lib/basic_libs').lib(global);
 
 // https://nodejs.org/api/process.html
 process.argv.forEach(function (val, index, array) {
-  console.log(index + ': ' + val);
+  // console.log(index + ': ' + val);
 });
 
 // fs => based on node js "File System" module
 fs.readFile(__dirname + '/package.json', 'utf8', function (err, file) {
   if(!err) {
-    console.log(JSON.parse(file).name);
+    // console.log(JSON.parse(file).name);
   }
 });
 
@@ -53,86 +53,88 @@ test.describe('Google Search - Test Tasks', function() {
     // console.log('end');
   });
 
-  // test.describe('Test Tasks 1-1, search for "webdriver"', function () {
-  //   var i = 0;
-  //   test.before(openBrowser);
+//   test.describe('Test Tasks 1-1, search for "webdriver"', function () {
+//     var i = 0;
+//     test.before(openBrowser);
 
-  //   test.after(closeBrowser);
+//     test.after(closeBrowser);
 
-  //   test.beforeEach(function () {
-  //     i++;
+//     test.beforeEach(function () {
+//       i++;
 
-  //     var google_obj = {
-  //       url: 'http://www.google.com/',
-  //       field: 'q',
-  //       keyword: 'webdriver',
-  //       btn: 'btnK'
-  //     };
+//       var google_obj = {
+//         url: 'http://www.google.com/',
+//         field: 'q',
+//         keyword: 'webdriver',
+//         btn: 'btnK'
+//       };
 
-  //     // Setup the basic data for the "search" function
-  //     setUp(this, google_obj);
+//       // Setup the basic data for the "search" function
+//       setUp(this, google_obj);
 
-  //     console.log('Start Test 1-1, Step ' + i);
-  //   });
+//       console.log('Start Test 1-1, Step ' + i);
+//     });
 
-  //   test.afterEach(function () {
-  //     console.log('End Test 1-1, Step ' + i);
-  //   });
+//     test.afterEach(function () {
+//       console.log('End Test 1-1, Step ' + i);
+//     });
 
-  //   // Step 1
-  //   test.it('Open "Chrome" browser', function() {
+//     // Step 1
+//     test.it('Open "Chrome" browser', function() {
 
-  //     // based on node js "EventEmitter"
-  //     EventEmitter.on('load', function () {
-  //       console.log('loaded');
-  //     });
+//       // based on node js "EventEmitter"
+//       EventEmitter.on('load', function () {
+//         console.log('loaded');
+//       });
 
-  //     // trigger the load event
-  //     EventEmitter.emit('load');
+//       // trigger the load event
+//       EventEmitter.emit('load');
 
-  //   });
+//     });
 
-  //   // Step 2
-  //   test.it('Set browser full screen', fullScreen);
+//     // Step 2
+//     test.it('Set browser full screen', fullScreen);
 
-  //   // Step 3
-  //   test.it('Enter "'+ this.url +'" url', sendUrl);
+//     // Step 3
+//     test.it('Enter "'+ this.url +'" url', sendUrl);
 
-  //   // Step 4
-  //   test.it('Show Current browser width', function() {
-  //     var js;
-  //     // Run my js code
-  //     js = 'return document.documentElement.clientWidth';
-  //     this.driver.executeScript(js).then(function (val) {
-  //       console.log('Current window width: ' + val + 'px');
-  //     });
-  //   });
+//     // Step 4
+//     test.it('Show Current browser width', function() {
+//       var js;
+//       // Run my js code
+//       js = 'return document.documentElement.clientWidth';
+//       this.driver.executeScript(js).then(function (val) {
+//         console.log('Current window width: ' + val + 'px');
+//       });
+//     });
 
-  //   // Step 5
-  //   test.it('Send for "webdriver" on search field', basic_actions.search);
+//     // Step 5
+//     test.it('Send for "webdriver" on search field', basic_actions.search);
 
-  //   // var name = 'Terry';
-  //   // use assert, change the assert message
-  //   // assert.equal(name, 'Terry', 'this name is not equal to "Terry"');
+//     // var name = 'Terry';
+//     // use assert, change the assert message
+//     // assert.equal(name, 'Terry', 'this name is not equal to "Terry"');
 
-  //   // Step 6
-  //   test.it('Check Current page title', function () {
-  //     this.driver.wait(checkTitle(this.keyword), 5000, 'checking title end.');
-  //     this.driver.getTitle().then(function (title) {
-  //       console.log('Current page title: ' + title);
-  //     });
-  //   });
+//     // Step 6
+//     test.it('Check Current page title', function () {
+//       this.driver.wait(checkTitle(this.keyword), 5000, 'checking title end.');
+//       this.driver.getTitle().then(function (title) {
+//         console.log('Current page title: ' + title);
+//       });
+//     });
 
-  // });
+//   });
 
   test.describe('Test Tasks 1-2, just guide yahoo page', function () {
     var url = 'http://www.yahoo.com.tw';
-    
+    // basic_actions = yahoo.basic_actions;
+
     test.before(openBrowser);
 
     test.after(closeBrowser);
 
     test.beforeEach(function () {
+      // Setup the basic data for the "search" function
       var yahoo_obj = {
         url: 'http://www.yahoo.com.tw',
         field: 'UHSearchBox',
@@ -140,8 +142,6 @@ test.describe('Google Search - Test Tasks', function() {
         btn: 'UHSearchWeb',
         site: 'yahoo'
       };
-
-      // Setup the basic data for the "search" function
       setUp(this, yahoo_obj);
     });
 
@@ -163,7 +163,14 @@ test.describe('Google Search - Test Tasks', function() {
 
     // Step 4
     test.it('Seach things', basic_actions.search);
-    console.log('fn: ' + basic_actions.search);
+
+    // Step 5
+    test.it('Check Current page title', function () {
+      this.driver.wait(checkTitle(this.keyword), 5000, 'checking title end.');
+      this.driver.getTitle().then(function (title) {
+        console.log('Current page title: ' + title);
+      });
+    });
   });
 
 });
